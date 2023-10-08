5013209fcd541cae41aa2fbcf0cd7449ae16e2a7
enoch <enirox001@gmail.com>
2023-10-08 13:40:56 +0100

    Initial Commit

---
+++ Vrtr/VrtrApp.swift
@@ -0,0 +1,17 @@
+//
+//  VrtrApp.swift
+//  Vrtr
+//
+//  Created by Ehnamuram Enoch on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct VrtrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
